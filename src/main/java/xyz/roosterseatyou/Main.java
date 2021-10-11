8b51573977e1269682dc9585360412e864cf2fd0
Roosterseatyou <ltbkthree@icloud.com>
2021-10-10 21:09:47 -0500

    Removed unused imports.

--- src/main/java/xyz/roosterseatyou/Main.java
+++ src/main/java/xyz/roosterseatyou/Main.java
@@ -1,13 +1,7 @@
 package xyz.roosterseatyou;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException, AWTException {
-        Frame frame = new Frame();
+    public static void main(String[] args) {
+        new Frame();
     }
 }
