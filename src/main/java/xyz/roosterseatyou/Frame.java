378979a55333ffe353ee007b421518a20f78410c
Roosterseatyou <ltbkthree@icloud.com>
2021-10-10 22:03:25 -0500

    Organized code a bit more, added comments, added an easter egg(????), and added a list maker to organize your passwords.

--- src/main/java/xyz/roosterseatyou/Frame.java
+++ src/main/java/xyz/roosterseatyou/Frame.java
@@ -6,38 +6,54 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Objects;
 import java.util.Random;
 
 public class Frame extends JFrame implements ActionListener {
     JButton button = new JButton();
+    JTextField websiteName = new JTextField();
     JTextField charBox = new JTextField();
     JTextPane passBox = new JTextPane();
+    JTextPane listBox = new JTextPane();
     JButton githubLink = new JButton();
     Frame(){
 
 
-        button.setBounds(400, 100, 200, 50);
-        passBox.setBounds(350, 200, 300, 20);
-        charBox.setBounds(400, 160, 200, 20 );
-        githubLink.setBounds(400, 600, 200, 50);
+        button.setBounds(100, 0, 200, 50);
+        websiteName.setBounds(50, 50, 300, 20);
+        charBox.setBounds(100, 80, 200, 20 );
+        passBox.setBounds(50, 110, 300, 20);
+        listBox.setBounds(400, 0, 550, 400);
+        githubLink.setBounds(860, 600, 200, 50);
+
+
         button.addActionListener(this);
+        githubLink.addActionListener(this);
+
         button.setText("Generate Password!");
         button.setBackground(Color.CYAN);
         button.setBorder(BorderFactory.createLineBorder(Color.GREEN, 2));
-        passBox.setBackground(Color.RED);
+
         charBox.setToolTipText("Character Amount");
+
+        passBox.setBackground(Color.RED);
         passBox.setEditable(false);
+
+        listBox.setEditable(false);
+
         githubLink.setText("Go to GitHub Repository");
-        githubLink.addActionListener(this);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setSize(1000,1000);
-        this.setVisible(true);
+        this.setSize(1080,1920);
         this.setBackground(Color.MAGENTA);
+        this.setVisible(true);
+
         this.add(button);
-        this.add(passBox);
+        this.add(websiteName);
         this.add(charBox);
+        this.add(passBox);
+        this.add(listBox);
         this.add(githubLink);
     }
     @Override
@@ -46,35 +62,60 @@ public class Frame extends JFrame implements ActionListener {
             Random rand = new Random();
             String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*=-_+|,<.>;:'~][{}()";
             StringBuilder pass = new StringBuilder();
+
             String charAmtStr = charBox.getText();
             int charAmtInt;
+            //Gets int from string and maybe adds an Easter egg????
             try {
                 charAmtInt = Integer.parseInt(charAmtStr);
             } catch (NumberFormatException err) {
-                passBox.setBackground(Color.RED);
-                passBox.setText("Not a valid Character Amount!");
+                //Rickroll
+                if(Objects.equals(charBox.getText(), "rickroll")){
+                    try {
+                        Desktop.getDesktop().browse(URI.create("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                //Error box
+                }else {
+                    passBox.setBackground(Color.RED);
+                    passBox.setText("Not a valid Character Amount!");
+                }
                 return;
             }
+            //Checks if character amount < 8 and fixes it
             if (charAmtInt < 8) {
-                passBox.setText("Must be more than 8 Characters for your own sake!");
                 charBox.setText("8");
                 charAmtInt = 8;
             }
+            //Checks if character amount is > 50 and fixes it
             if (charAmtInt > 50) {
-                passBox.setText("You do not need a password with >50 Characters!");
                 charBox.setText("50");
                 charAmtInt = 50;
             }
+
             for (int i = 0; i < charAmtInt; i++) {
                 char rChar = characters.charAt(rand.nextInt(characters.length()));
                 pass.append(rChar);
             }
-            if (e.getSource() == button) {
-                button.setText("Generate New Password!");
-                passBox.setText(pass.toString());
-                passBox.setBackground(Color.GREEN);
+
+            button.setText("Generate New Password!");
+            passBox.setText(pass.toString());
+            passBox.setBackground(Color.GREEN);
+
+            //List maker
+            String webName = websiteName.getText();
+            String listBoxStr = listBox.getText();
+
+            if(listBoxStr == null) {
+                listBox.setText(webName + ": " + pass);
+            }else {
+                listBox.setText(listBoxStr + "\n" + webName + ": " + pass);
             }
+
         }else
+            //GitHub button
         if(e.getSource() == githubLink){
             try {
                 Desktop.getDesktop().browse(URI.create("https://github.com/Roosterseatyou/PasswordGenerator"));
@@ -83,5 +124,4 @@ public class Frame extends JFrame implements ActionListener {
             }
         }
     }
-
 }
